edf3ed731396d8454cdcbdc7c591a1933989d1c7
Qkessler <qkessler@gmail.com>
2022-12-20 20:08:51 +0100

    Add taplo lsp, for toml files

--- modules/qk-eglot.el
+++ modules/qk-eglot.el
@@ -116,7 +116,9 @@ server getting expensively restarted when reverting buffers."
                     "-Xmx8G"
                     "-XX:+UseG1GC"
                     "-XX:+UseStringDeduplication"
-                    ,(concat "--jvm-arg=-javaagent:" (getenv "HOME") "/.lombok/lombok.jar")))))
+                    ,(concat "--jvm-arg=-javaagent:" (getenv "HOME") "/.lombok/lombok.jar"))))
+  (add-to-list 'eglot-server-programs
+               `(toml-ts-mode . ("taplo" "lsp" "stdio"))))
 
 (use-package eldoc-box
   :straight t
